52b19d54a1b5e566558b9052e7e9e52e0dbe2404
Thania Vivek <tvivek1@sheffield.ac.uk>
2024-05-17 14:17:22 +0100

    modified package.json

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/PortfolioWebsite/', // This should match the repository name
   plugins: [react()],
-})
+});
